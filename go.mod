3d309f6f33363797a84b6ee3f6e762029d2a7e6f
Vladimir Belomestnykh <work.pwnz+github@gmail.com>
2022-01-08 18:47:20 +0500

    1. Added main functional
    Signed-off-by: Vladimir Belomestnykh <work.pwnz+github@gmail.com>

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module ping_wrapper
 go 1.16
 
 require (
-	github.com/go-ping/ping v0.0.0-20210911151512-381826476871 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/go-ping/ping v0.0.0-20210911151512-381826476871
 	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf // indirect
 	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
 )
--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-const version string = "0.1.0"
+const version string = "0.2.0"
 
 type Flags struct {
 	name  string
@@ -21,47 +21,65 @@ type Flags struct {
 }
 
 var (
+	myFlag = flag.NewFlagSet("myFlagSet", 1)
 	// input args
 	IP = Flags{"IP", "dns.google",
 		"IPv4 address or Domain name. Example: 8.8.8.8 or dns.google"}
-	IPv4 = flag.String(IP.name, IP.value, IP.usage)
+	IPv4 = myFlag.String(IP.name, IP.value, IP.usage)
 
 	CountF    = Flags{"C", "5", "Number of echo requests to send"}
 	CountV, _ = strconv.Atoi(CountF.value)
-	Count     = flag.Int(CountF.name, CountV, CountF.usage)
+	Count     = myFlag.Int(CountF.name, CountV, CountF.usage)
 
 	IntervalF    = Flags{"I", "1", "Interval is the wait time between each packet send. Default is 1s."}
 	IntervalV, _ = strconv.Atoi(IntervalF.value)
-	Interval     = flag.Int(IntervalF.name, IntervalV, IntervalF.usage)
+	Interval     = myFlag.Int(IntervalF.name, IntervalV, IntervalF.usage)
 
 	TimeoutF = Flags{"T", "-1",
 		"Timeout specifies a timeout before ping exits, regardless of how many packets have been receive"}
 	TimeoutV, _ = strconv.Atoi(TimeoutF.value)
-	Timeout     = flag.Int(TimeoutF.name, TimeoutV, TimeoutF.usage)
+	Timeout     = myFlag.Int(TimeoutF.name, TimeoutV, TimeoutF.usage)
 
 	PacketSizeF    = Flags{"L", "24", "Size of packet being sent"}
 	PacketSizeV, _ = strconv.Atoi(PacketSizeF.value)
-	PacketSize     = flag.Int(PacketSizeF.name, PacketSizeV, PacketSizeF.usage)
+	PacketSize     = myFlag.Int(PacketSizeF.name, PacketSizeV, PacketSizeF.usage)
 
 	SourceF = Flags{"S", "", "Source is the source IP address"}
-	Source  = flag.String(SourceF.name, SourceF.value, SourceF.usage)
-
+	Source  = myFlag.String(SourceF.name, SourceF.value, SourceF.usage)
 	// output args
-	ArgsF = Flags{"O", "", "Returns one argument from the returned result"}
-	Args  = flag.String(ArgsF.name, ArgsF.value, ArgsF.usage)
+	OutputF = Flags{"O", "", "Returns one argument from the returned result"}
+	Output  = myFlag.String(OutputF.name, OutputF.value, OutputF.usage)
 
-	RawF    = Flags{"R", "false", "Display RAW result. Incompatible with argument 'O'"}
-	RawV, _ = strconv.ParseBool(RawF.value)
-	Raw     = flag.Bool(RawF.name, RawV, RawF.usage)
+	RawF = Flags{"R", "false", "Display RAW result. Incompatible with argument 'O'"}
+	Raw  = myFlag.String(RawF.name, RawF.value, RawF.usage)
+
+	OutArgs = []string{"Availability", "Min", "Max", "Std", "Loss", "Sent", "Recv"}
 )
+
+// Overriding default help
+type myFlagSet struct {
+	*flag.FlagSet
+}
+
+func (f *myFlagSet) PrintDefaults() {
+	PrintDefaults()
+}
 
 func PrintDefaults() {
 	spacesl1 := strings.Repeat(" ", 2)
 	spacesl2 := spacesl1 + strings.Repeat(" ", 1)
 	spacesl3 := spacesl2 + strings.Repeat(" ", 6)
 	fmt.Printf(spacesl1 + "Input args:\n")
-	fmt.Printf(spacesl2+"-%s %T\n%s%s", IP.name, IP.name, spacesl3, IP.usage)
-
+	fmt.Printf(spacesl2+"-%s %T\n%s%s (default \"%s\")\n", IP.name, IP.name, spacesl3, IP.usage, IP.value)
+	fmt.Printf(spacesl2+"-%s %T\n%s%s (default %s)\n", CountF.name, CountF.name, spacesl3, CountF.usage, CountF.value)
+	fmt.Printf(spacesl2+"-%s %T\n%s%s (default %s)\n", PacketSizeF.name, PacketSizeF.name, spacesl3, PacketSizeF.usage, PacketSizeF.value)
+	fmt.Printf(spacesl2+"-%s %T\n%s%s (default %s)\n", IntervalF.name, IntervalF.name, spacesl3, IntervalF.usage, IntervalF.value)
+	fmt.Printf(spacesl2+"-%s %T\n%s%s (default %s)\n", TimeoutF.name, TimeoutF.name, spacesl3, TimeoutF.usage, TimeoutF.value)
+	fmt.Printf(spacesl2+"-%s %T\n%s%s (default \"%s\")\n", SourceF.name, SourceF.name, spacesl3, SourceF.usage, SourceF.value)
+	fmt.Printf(spacesl1 + "Output args:\n")
+	fmt.Printf(spacesl2+"-%s %T\n%s%s (default \"%s\")\n", OutputF.name, OutputF.name, spacesl3, OutputF.usage, OutputF.value)
+	fmt.Printf(spacesl3+"Can use one of the keys: %v\n", strings.Join(OutArgs, ", "))
+	fmt.Printf(spacesl2+"-%s %T\n%s%s (default %s)\n", RawF.name, RawF.name, spacesl3, RawF.usage, RawF.value)
 }
 
 var Usage = func() {
@@ -70,14 +88,19 @@ var Usage = func() {
 }
 
 func main() {
-	flag.Usage = Usage
-	// Init variables
 
-	//var PacketsSent = flag.Int()
-	flag.Parse()
+	var _flag = myFlagSet{myFlag}
+	_flag.Usage = Usage
+	err := _flag.Parse(os.Args[1:])
+
+	if err != nil {
+		//panic(err)
+	}
+
 	if len(os.Args) <= 1 {
-		flag.PrintDefaults()
-	} else if *Args != "" && *Raw != false {
+		fmt.Printf("Usage of ping_wrapper (ver %s) [<input args>] [<output args>]\n", version)
+		PrintDefaults()
+	} else if *Output != "" && *Raw != "false" {
 		fmt.Println("Output parameters can't have RAW and processed format at the same time")
 	} else {
 		pinger, err := ping.NewPinger(*IPv4)
@@ -95,8 +118,7 @@ func main() {
 		if *PacketSize != 24 {
 			pinger.Size = *PacketSize
 		}
-		// добавить валидацию на IP
-		// добавить параметры выхода
+
 		if *Source != "" {
 			result, err := ipv4Validator(*Source)
 			if err != nil {
@@ -119,28 +141,49 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		if *Args == "Avg" {
-			fmt.Println(pinger.Statistics().AvgRtt)
-		} else if *Args == "Min" {
-			fmt.Println(pinger.Statistics().MinRtt)
-		} else if *Args == "Max" {
-			fmt.Println(pinger.Statistics().MaxRtt)
-		} else if *Args == "Std" {
-			fmt.Println(pinger.Statistics().StdDevRtt)
-		} else if *Args == "Loss" {
-			fmt.Println(pinger.Statistics().PacketLoss)
-		} else if *Args == "Sent" {
-			fmt.Println(pinger.Statistics().PacketsSent)
-		} else if *Args == "Recv" {
-			fmt.Println(pinger.Statistics().PacketsRecv)
-		} else if *Args == "Availability" {
-			if pinger.Statistics().PacketLoss == 0 {
-				fmt.Println("1")
-			} else if pinger.Statistics().PacketLoss == 100 {
-				fmt.Println("0")
+
+		if len(*Output) >= 1 {
+			var correctRegistry = strings.ToLower(*Output)
+			correctRegistry = strings.ToUpper(string(correctRegistry[0])) + correctRegistry[1:]
+
+			ok := false
+			for _, v := range OutArgs {
+				if correctRegistry == v {
+					ok = true
+				}
 			}
-		} else {
-			fmt.Println("Sorry, but specified parameter is unknown")
+			if ok != true {
+				errors.New(fmt.Sprintf("Incorrect argunemt"))
+				os.Exit(1)
+			}
+
+			if correctRegistry == "Avg" {
+				fmt.Println(pinger.Statistics().AvgRtt)
+			} else if correctRegistry == "Min" {
+				fmt.Println(pinger.Statistics().MinRtt)
+			} else if correctRegistry == "Max" {
+				fmt.Println(pinger.Statistics().MaxRtt)
+			} else if correctRegistry == "Std" {
+				fmt.Println(pinger.Statistics().StdDevRtt)
+			} else if correctRegistry == "Loss" {
+				fmt.Println(pinger.Statistics().PacketLoss)
+			} else if correctRegistry == "Sent" {
+				fmt.Println(pinger.Statistics().PacketsSent)
+			} else if correctRegistry == "Recv" {
+				fmt.Println(pinger.Statistics().PacketsRecv)
+			} else if correctRegistry == "Availability" {
+				if pinger.Statistics().PacketLoss == 0 {
+					fmt.Println("1")
+				} else if pinger.Statistics().PacketLoss == 100 {
+					fmt.Println("0")
+				}
+			} else {
+				fmt.Println("Sorry, but specified parameter is unknown")
+			}
+		} else if len(*Output) == 0 && *Raw == "true" {
+			fmt.Printf("RAW output -> %v", *pinger.Statistics())
+		} else if len(*Output) == 0 && *Raw != "true" {
+			fmt.Println("Specify an output parameter '-O' or '-R'!")
 		}
 
 	}
